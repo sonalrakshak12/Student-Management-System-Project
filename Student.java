3ba469f5b8e987b034a409e8203c1bdbe3b339fa
Sonal Rakshak <sonalrakshak12@gmail.com>
2026-06-09 10:42:19 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+
+    public Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    public String toString() {
+        return "\nStudent Details" +
+               "\nID      : " + id +
+               "\nName    : " + name +
+               "\nAge     : " + age +
+               "\nCourse  : " + course;
+    }
+}
